09e2263aa866d043a36a594c801bd085d73f8b84
Thomas Hancock <thomas.ian.hancock@gmail.com>
2012-02-26 19:50:04 +0000

    Changed output file.

--- get_problem_solution_times.sh
+++ get_problem_solution_times.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
-date=`date +%d%m%y`
-output_file=execution_times_$date.txt 
+output_file=execution_times.txt 
 
-if [ -e execution_times/$output_file ]; then
-    rm execution_times/$output_file
+if [ -e $output_file ]; then
+    rm $output_file
 fi
 
 for file in `ls -v [0-9]*.py`; do
     problem_num=${file%.*}
     execution_time=`(/usr/bin/time --format="%E" python $file) 2>&1 1>/dev/null`
-    echo "$problem_num: $execution_time" >> execution_times/$output_file
+    echo "$problem_num: $execution_time" >> $output_file
 done
