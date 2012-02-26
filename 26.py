25512c5eebcfd79aa50b5c79f43b1f52f05e1fc6
Thomas Hancock <thomas.ian.hancock@gmail.com>
2012-02-26 17:53:05 +0000

    Performance improvements for problem 26.

--- 26.py
+++ 26.py
@@ -1,34 +1,22 @@
 import re
 from fractions import Fraction
-from decimal import Decimal
+from decimal import *
+
+getcontext().prec=2000
 
 largest_len=0
 result=0
 r = re.compile(r"([0-9]{7,}?)\1+")
 
 for n in range (2, 1000):
-    s_division=""
-    num=1
-    re_len=0
+    unit_fraction=Decimal(1)/Decimal(n)
 
-    while num < n:
-        num*=10
+    matches=r.findall(str(unit_fraction))
+    if len(matches) > 0:
+        pattern=max(matches)
 
-    while len(r.findall(s_division))==0:
-        if num < n:
-            next_num=0
-            num*=10
-        else:
-            next_num=int(num/n)
-            num%=n
-            num*=10
-
-        s_division+=str(next_num)
-
-    print n
-
-    if len(r.findall(s_division)[0]) > largest_len:
-        largest_len=len(r.findall(s_division)[0])
-        result=n
+        if len(pattern) > largest_len:
+            largest_len=len(pattern)
+            result=n
 
 print result
