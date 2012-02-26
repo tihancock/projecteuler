c6437a3d3270994d94312a86c242f843caa7532e
Thomas Hancock <thomas.ian.hancock@gmail.com>
2012-02-26 19:17:38 +0000

    Updated timing script

--- get_problem_solution_times.sh
+++ get_problem_solution_times.sh
@@ -3,12 +3,12 @@
 date=`date +%d%m%y`
 output_file=execution_times_$date.txt 
 
-if [ -e $output_file ]; then
-    rm $output_file
+if [ -e execution_times/$output_file ]; then
+    rm execution_times/$output_file
 fi
 
 for file in `ls -v [0-9]*.py`; do
     problem_num=${file%.*}
-    echo "problem $problem_num" >> $output_file
-    echo `(/usr/bin/time --format="%E" python $file) >> $output_file 2>&1 1>/dev/null`; 
+    execution_time=`(/usr/bin/time --format="%E" python $file) 2>&1 1>/dev/null`
+    echo "$problem_num: $execution_time" >> execution_times/$output_file
 done
