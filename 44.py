1e02692498178703964608f8005893c706dbdce9
Thomas Hancock <thomas.ian.hancock@gmail.com>
2012-02-27 13:46:39 +0000

    Performance improvements for problem 44

--- 44.py
+++ 44.py
@@ -1,12 +1,9 @@
-Pentagons=[n*(3*n-1)/2 for n in range(1,100000)]
-Pentagon_set=set(Pentagons)
-#print [p for p in Pentagons and q for q in Pentagons if p+q in set(Pentagons) and p-q in set(Pentagons)]
+import sys
 
-print Pentagons[-1]
+Pentagons=set([n*(3*n-1)/2 for n in range(1,5000)])
 
 for p in Pentagons:
     for q in Pentagons:
-        if p+q in Pentagon_set and p-q in Pentagon_set:
-            print  abs(p-q)
-
-    print p
+        if p+q in Pentagons and p-q in Pentagons:
+            print abs(p-q)
+            sys.exit()
