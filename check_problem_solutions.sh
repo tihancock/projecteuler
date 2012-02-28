ee2803e3c120fb47882cc0eb3bc752b1093c63cb
Thomas Hancock <thomas.ian.hancock@gmail.com>
2012-02-28 21:26:52 +0000

    Script designed to regression test against refactored solutions.

---
+++ check_problem_solutions.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+output_file=solutions.txt 
+
+if [ -e $output_file ]; then
+    rm $output_file
+fi
+
+for file in `ls -v [0-9]*.py`; do
+    problem_num=${file%.*}
+    echo $problem_num
+    solution=`python $file`
+    echo "$problem_num: $solution" >> $output_file
+done
+
+difference=`diff master_solutions.txt $output_file`
+
+if [ difference=="" ]; then
+    echo "All solutions correct!"
+else
+    echo "The following solution attempts are incorrect:"
+    echo diff
+done
