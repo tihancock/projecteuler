c00df5a5dbf394df22119575d6ddb88b48c1d6fc
Tom Hancock <thomas.ian.hancock@gmail.com>
2013-12-09 20:47:27 +0000

    More clojure solutions

--- clojure/project-euler/src/project_euler/core.clj
+++ clojure/project-euler/src/project_euler/core.clj
@@ -41,13 +41,13 @@
 (apply max (map #(reduce * %) (partition 5 1 (map #(Integer/parseInt %) (rest (string/split s #""))))))
 
 ;; 9
-;; FIXME
+;; TODO
 
 ;; 10
 (reduce + (take-while #(< % 2e6) (primes/lazy-primes)))
 
 ;; 11
-;; FIXME
+;; TODO
 
 ;; 12
 (defn num-factors
@@ -181,7 +181,75 @@
 (max-collatz-length 1e6)
 
 ;; 15
-;; FIXME - simple factorial
+;; TODO - simple factorial
 
 ;; 16
 (reduce + (map #(Integer/parseInt %) (rest (string/split (str (reduce * (repeat 1000 (bigint 2)))) #""))))
+
+;; 17
+;; TODO
+
+;; 18
+(def triangle 
+  [[75]
+   [95 64]
+   [17 47 82]
+   [18 35 87 10]
+   [20 4  82 47 65]
+   [19 1  23 75 3  34]
+   [88 2  77 73 7  63 67]
+   [99 65 4  28 6  16 70 92]
+   [41 41 26 56 83 40 80 70 33]
+   [41 48 72 33 47 32 37 16 94 29]
+   [53 71 44 65 25 43 91 52 97 51 14]
+   [70 11 33 28 77 73 17 78 39 68 17 57]
+   [91 71 52 38 17 14 91 43 58 50 27 29 48]
+   [63 66 4  68 89 53 67 30 73 16 69 87 40 31]
+   [4  62 98 27 23 9  70 98 73 93 38 53 60 4  23]])
+
+(defn max-triangle-sum
+  [inverted-triangle]
+  (if (= (count inverted-triangle) 1)
+    (flatten inverted-triangle)
+    (let [low-pairs (partition 2 1 (first inverted-triangle))
+          low-maxes (map #(apply max %) low-pairs)
+          new-first-row (map + low-maxes (first (rest inverted-triangle)))]
+      (max-triangle-sum (cons new-first-row (drop 2 inverted-triangle))))))
+
+(max-triangle-sum (reverse triangle))
+
+;; 20
+(defn factorial
+  [x]
+  (if (<= x 1)
+    1
+    (* x (factorial (dec x)))))
+
+(reduce + (map #(Integer/parseInt %) (rest (string/split (str (factorial 100N)) #""))))
+
+;; 21
+(defn sum-proper-divisors
+  [x]
+  (let [pfs (primes/prime-factors x)
+        freqs (frequencies pfs)]
+    (int (- (reduce * (for [[k v] freqs] (/ (- (Math/pow k (+ v 1)) 1) (- k 1)))) x))))
+
+(defn sum
+  [s]
+  (reduce + s))
+
+(let [ks (range 1 10000)
+      vs (map #(sum-proper-divisors %) ks)
+      m (apply hash-map (interleave ks vs))
+      amicables (select-keys m (for [[k v] m :when (and (= (m v) k) (not= k v))] k))]
+  (reduce + (vals amicables)))
+
+;; 22
+(let [sorted-names (sort (string/split (string/replace (slurp "data/names.txt") #"\"" "") #","))
+      name-values (map (fn [n] (reduce + (map #(- % 64) (map int n)))) sorted-names)
+      pos-name-values (map * name-values (iterate inc 1))]
+  (reduce + pos-name-values))
+
+;; 23
+(let [abundants (set (filter #(> (reduce + (proper-divisors %)) %) (range 281)))]
+  (first abundants))
